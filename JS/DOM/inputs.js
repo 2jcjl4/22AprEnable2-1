11b8e4d093fd266a6b14bbfeefa392e3a953bf14
James <2jcjl4@gmail.com>
2022-05-20 13:39:54 +0100

    calculator in Html

--- JS/DOM/inputs.js
+++ JS/DOM/inputs.js
@@ -7,6 +7,9 @@ let inputTwo = document.querySelector("#inputTwo");
 
 // Buttons
 let addBtn = document.querySelector("#addBtn");
+let subBtn = document.querySelector("#subBtn");
+let divBtn = document.querySelector("#divBtn");
+let multiBtn = document.querySelector("#multiBtn");
 let clearBtn = document.querySelector("#clearBtn");
 
 // Divs
@@ -23,6 +26,36 @@ let add = () => {
     valueDiv.appendChild(myValue);
     mainDiv.appendChild(valueDiv);
 }
+let sub = () => {
+    let valueOne = parseInt(inputOne.value);
+    let valueTwo = parseInt(inputTwo.value);
+
+    let valueDiv = document.createElement("div");
+    let myValue = document.createTextNode(`${valueOne} - ${valueTwo} = ${valueOne - valueTwo}`);
+
+    valueDiv.appendChild(myValue);
+    mainDiv.appendChild(valueDiv);
+}
+let div = () => {
+    let valueOne = parseInt(inputOne.value);
+    let valueTwo = parseInt(inputTwo.value);
+
+    let valueDiv = document.createElement("div");
+    let myValue = document.createTextNode(`${valueOne} / ${valueTwo} = ${valueOne / valueTwo}`);
+
+    valueDiv.appendChild(myValue);
+    mainDiv.appendChild(valueDiv);
+}
+let multi = () => {
+    let valueOne = parseInt(inputOne.value);
+    let valueTwo = parseInt(inputTwo.value);
+
+    let valueDiv = document.createElement("div");
+    let myValue = document.createTextNode(`${valueOne} * ${valueTwo} = ${valueOne * valueTwo}`);
+
+    valueDiv.appendChild(myValue);
+    mainDiv.appendChild(valueDiv);
+}
 
 let clearResults = () => {
     mainDiv.innerHTML = "";
@@ -30,4 +63,7 @@ let clearResults = () => {
 
 // Event Listeners
 addBtn.addEventListener("click", add);
+subBtn.addEventListener("click", sub);
+divBtn.addEventListener("click", div);
+multiBtn.addEventListener("click", multi);
 clearBtn.addEventListener("click", clearResults);
